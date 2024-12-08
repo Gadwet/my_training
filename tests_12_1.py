4eb79d20c779ee84944a366fee9b57dd431dc834
Gadwet <gadwet9929@mail.ru>
2024-12-08 18:59:52 +0300

    Add files via upload

--- tests_12_1.py
+++ tests_12_1.py
@@ -2,23 +2,23 @@ import unittest
 import module_12_1
 
 class RunnerTest(unittest.TestCase):
-    is_frozen = False
+    #is_frozen = False
 
-    @unittest.skipIf(is_frozen, None)
+    #@unittest.skipIf(is_frozen, None)
     def test_walk(self):
         walks = module_12_1.Runner('Денис')
         for i in range(10):
             walks.walk()
         self.assertEqual(walks.distance, 50)
 
-    @unittest.skipIf(is_frozen, None)
+    #@unittest.skipIf(is_frozen, None)
     def test_run(self):
         runs = module_12_1.Runner('Николай')
         for i in range(10):
             runs.run()
         self.assertEqual(runs.distance, 100)
 
-    @unittest.skipIf(is_frozen, None)
+    #@unittest.skipIf(is_frozen, None)
     def test_challenge(self):
         runs = module_12_1.Runner('Евгений')
         walks = module_12_1.Runner('Алексей')
--- tests_12_2.py
+++ tests_12_2.py
@@ -2,13 +2,13 @@ import module_12_2 as m12
 import unittest
 
 class TournamentTest(unittest.TestCase):
-    is_frozen = True
+    #is_frozen = True
 
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    #@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def setUp(self):
         self.r1 = m12.Runner('Усейн', 10)
         self.r2 = m12.Runner('Андрей', 9)
@@ -22,7 +22,7 @@ class TournamentTest(unittest.TestCase):
                 show_result[place] = runner.name
             print(show_result)
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    #@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run_1(self):
         self.tournament = m12.Tournament(90, self.r1, self.r3)
         self.all_results = self.tournament.start()
@@ -30,7 +30,7 @@ class TournamentTest(unittest.TestCase):
         self.assertTrue(last_runner_name == 'Ник')
         TournamentTest.all_results[1] = self.all_results
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    #@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run_2(self):
         self.tournament_2 = m12.Tournament(90, self.r2, self.r3)
         self.all_results = self.tournament_2.start()
@@ -38,7 +38,7 @@ class TournamentTest(unittest.TestCase):
         self.assertTrue(last_runner_name == 'Ник')
         TournamentTest.all_results[2] = self.all_results
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    #@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run_3(self):
         self.tournament_3 = m12.Tournament(90, self.r1, self.r2, self.r3)
         self.all_results = self.tournament_3.start()
